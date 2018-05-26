a7af4ce97d63c2055de4ac283e0061a736109d25
TRAING Serey <t.sereypich@gmail.com>
2018-05-26 17:00:05 +0200

    fix import

--- src/pages/homepage/homepage.ts
+++ src/pages/homepage/homepage.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { Launch } from '../../models/Launch';
+import { Launch } from '../../models/launchs/Launch';
 import { SpaceXApiProvider } from '../../providers/space-x-api/space-x-api';
 
 /**
